5d9bdd03a60399b57b48139a16cec621f45ab63d
Antonin Hackenberg <antonin.hackenberg@gmail.com>
2016-09-29 10:06:07 +0200

    Init app

---
+++ src/js/index.jsx
@@ -0,0 +1,22 @@
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import ListContainer from './components/TodoListContainer/index';
+import store from './stores';
+import './index.css';
+
+const App = () => (
+  <div className="main">
+    <h3> Todo List </h3>
+    <ListContainer />
+  </div>
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  window.document.getElementById('root')
+);
